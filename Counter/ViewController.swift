11ca3ae0406108441471c557386607eb73f2e6a2
Vladimir Luzhbin <vladlug@gmail.com>
2024-02-29 14:46:08 +0100

    Выполненное упрощенное задание для спринта 3

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var mycount: Int8 = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        
     }
+    func viewIsAppearing() {
+        mycount = 0
+        CounterLabel.text = "Значение счётчика: \(mycount)"
+    }
+    
+    
+    @IBOutlet weak var CounterLabel: UILabel!
+    
+    
+    @IBAction func CountButton(_ sender: Any) {
+        mycount += 1
+        CounterLabel.text = "Значение счётчика: \(mycount)"
+        
+}
+
 
 
 }
-
